998aef065e3fbd0dfb3bc1485d21d259e5deca4c
Pablo Servile <pablo.mcd@hotmail.com>
2019-05-07 21:03:34 -0300

    ended tests

--- src/test/java/ar/com/flexibility/examen/test/CustomerTests.java
+++ src/test/java/ar/com/flexibility/examen/test/CustomerTests.java
@@ -1,6 +1,8 @@
 package ar.com.flexibility.examen.test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -15,14 +17,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
 
 import ar.com.flexibility.examen.domain.entity.Customer;
 import ar.com.flexibility.examen.domain.service.CustomerService;
 
-public class ApplicationDocumentationTest {
+public class CustomerTests {
 
-	private MockMvc mockMvc;
 	private Customer customer;
 
 	@Mock
@@ -40,56 +40,45 @@ public class ApplicationDocumentationTest {
 		customer.setBirthdate(birthdate);
 		customer.setEmail("mail@mail.com");
 		customer.setSurname("Ruben");
+		customer.setId(1L);
 
 		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test
-	public void testListAllUsers() throws Exception {
-
+	public void testListAllCustomers() throws Exception {
 		List<Customer> list = new ArrayList<Customer>();
-		Customer empOne = new Customer();
-		Customer empTwo = new Customer();
-		Customer empThree = new Customer();
-
-		list.add(empOne);
-		list.add(empTwo);
-		list.add(empThree);
-
+		Customer custOne = new Customer();
+		Customer custTwo = new Customer();
+		Customer custThree = new Customer();
+		list.add(custOne);
+		list.add(custTwo);
+		list.add(custThree);
 		when(customerService.listCustomers()).thenReturn(list);
-
-		List<Customer> empList = customerService.listCustomers();
-
-		assertEquals(3, empList.size());
+		List<Customer> custList = customerService.listCustomers();
+		assertEquals(3, custList.size());
 		verify(customerService, times(1)).listCustomers();
-
 	}
 
 	@Test
 	public void testSaveCustomer() throws Exception {
 		when(customerService.save(customer)).thenReturn(customer);
-
 		Customer customTest = customerService.save(customer);
-
-		assertEquals();
-
+		assertEquals(1, customTest.getId().longValue());
 	}
 
 	@Test
 	public void testFindCustomerById() throws Exception {
-
 		when(customerService.findById(1L)).thenReturn(customer);
 		Customer customTest = customerService.findById(1L);
-		assertEquals();
-
+		assertNotNull(customTest);
 	}
 
 	@Test
 	public void testDeleteCustomer() throws Exception {
 		when(customerService.delete(1L)).thenReturn(true);
-
-		assertEquals();
-
+		boolean value = customerService.delete(1L);
+		assertTrue(value);
 	}
 
 }
--- src/test/java/ar/com/flexibility/examen/test/ProcessMessageServiceTest.java
+++ src/test/java/ar/com/flexibility/examen/test/ProcessMessageServiceTest.java
@@ -1,27 +1,25 @@
 package ar.com.flexibility.examen.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+
 import ar.com.flexibility.examen.domain.model.Message;
 import ar.com.flexibility.examen.domain.service.impl.ProcessMessageServiceImpl;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-
-@RunWith(MockitoJUnitRunner.class)
 public class ProcessMessageServiceTest {
 
-    @InjectMocks
-    private ProcessMessageServiceImpl messageService;
+	@InjectMocks
+	private ProcessMessageServiceImpl messageService;
 
-    @Test
-    public void processMessage()
-    {
-        String messageTest = "TEST";
-        Message message = messageService.processMessage(messageTest);
+	@Test
+	public void processMessage() {
+		String messageTest = "TEST";
+		Message message = messageService.processMessage(messageTest);
 
-        assertNotNull(message);
-        assertEquals(message.getMessage(), messageTest);
-    }
+		assertNotNull(message);
+		assertEquals(message.getMessage(), messageTest);
+	}
 }
